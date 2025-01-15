72afda08e9bf1f561444053913c783db896f3a6b
Doms <dlmagat08@proton.me>
2025-01-15 21:47:32 +0800

    cached and live api with next.js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,12 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { STARTUP_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
 
     const query = (await searchParams).query
-    const posts = await client.fetch(STARTUP_QUERY)
+    const { data: posts } = await sanityFetch({ query: STARTUP_QUERY })
 
     return (
         <>
@@ -35,6 +35,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
                 </ul>
             </section>
 
+            <SanityLive />
         </>
     );
 }
