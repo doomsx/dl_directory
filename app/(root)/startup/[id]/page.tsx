073248d52e40dd274ad649711f7aadca0dc411f1
Doms <dlmagat08@proton.me>
2025-01-17 21:59:31 +0800

    ppr

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -3,6 +3,8 @@ import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
+export const experimental_ppr = true
+
 const StartupDetails = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id
 
