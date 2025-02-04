ccb076f26aefa9a0fa2c0aaa3cbd7191db6c3ce5
Doms <dlmagat08@proton.me>
2025-02-04 20:00:14 +0800

    user profile

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -14,7 +14,17 @@ const UserPage = async ({ params }: { params: Promise<{ id: string }> }) => {
         return notFound()
     }
     return (
-        <></>
+        <>
+            <section className='profile_container'>
+                <div className='profile_card'>
+                    <div className='profile_title'>
+                        <h3 className='text-24-black uppercase text-center line-clamp-1'>
+                            {user.name}
+                        </h3>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
